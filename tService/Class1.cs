8a31058352126cca945837e5b3e90d7a0defe10a
AleksanderGalkin <agalkinov@mail.ru>
2015-01-31 19:24:56 +0700

    Добавил решение в Гит

---
+++ tService/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace MyService    
+{
+
+        [ServiceContract]
+        public interface IMyService
+        {
+            [OperationContract]
+            string GetData(string name);
+        }
+
+        public class MyService : IMyService
+        {
+            public string GetData(string name)
+            {
+                switch (name)
+                {
+                    case "Galkin":
+                        return "Aleksandr";
+                    case "Galkina":
+                        return "Viktoria";
+                    default:
+                        return name + " is undefined";
+                }
+            }
+        }
+}
