2291a7b3cb4bd9262123bbdc6def0dbedac6906e
AleksanderGalkin <agalkinov@mail.ru>
2015-02-27 22:13:41 +0700

    Модульные тесты

--- tServiceREST/MethodTst.cs
+++ tServiceREST/MethodTst.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using MyService;
+using tServiceREST;
 
 namespace Tests
 {
+    
 
     [TestFixture]
     public class TestMyService
     {
+        private DhBmObj inData;
+
+        public TestMyService()
+        {
+            Point [] pointsDh = new [] { new Point(){x = 1,y=2,z=3, cr=1, info="test"}
+                                        ,new Point(){x = 2,y=3,z=4, cr=2, info="test"}
+                                        ,new Point(){x = 3,y=4,z=5, cr=3, info="test"}
+            };
+            Point[] pointsBm = new[] {   new Point(){x = 2,y=3,z=4}
+                                        ,new Point(){x = 3,y=4,z=5}
+                                        ,new Point(){x = 5,y=5,z=4}
+            };
+            DhObj inDh = new DhObj() { points=pointsDh };
+            BmObj inBm = new BmObj() { points=pointsBm, xAxis=20, yAxis=10, zAxis=10, xElPos=1, yElPos=4, zElPos=4};
+            inData = new DhBmObj() { dh=inDh, bm=inBm };
+
+        }
+
         [Test]
         public void Test()
         {
